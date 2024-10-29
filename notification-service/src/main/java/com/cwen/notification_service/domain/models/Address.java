61261addb1d7d70dc150d9d09631687d7d88b64f
Chu Hao Wen <chuhaow@gmail.com>
2024-10-29 16:51:03 -0500

    skeleton set up

---
+++ notification-service/src/main/java/com/cwen/notification_service/domain/models/Address.java
@@ -0,0 +1,12 @@
+package com.cwen.notification_service.domain.models;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record Address(
+        @NotBlank(message = "Must have a primary address") String addressLine1,
+        String addressLine2,
+        @NotBlank(message = "City is required") String city,
+        @NotBlank(message = "State is required") String state,
+        @NotBlank(message = "ZipCode is required") String zipCode,
+        @NotBlank(message = "Country is required") String country
+) { }
---
+++ notification-service/src/test/java/com/cwen/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cwen.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+
+class NotificationServiceApplicationTests  extends  AbstractIntegrationTest{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
