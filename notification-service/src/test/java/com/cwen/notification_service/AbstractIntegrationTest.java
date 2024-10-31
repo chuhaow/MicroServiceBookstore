3016c6c25ff118d5a32f44781be00586649dbf12
Chu Hao Wen <chuhaow@gmail.com>
2024-10-31 16:03:23 -0500

    add tests

--- notification-service/src/test/java/com/cwen/notification_service/AbstractIntegrationTest.java
+++ notification-service/src/test/java/com/cwen/notification_service/AbstractIntegrationTest.java
@@ -1,24 +1,16 @@
 package com.cwen.notification_service;
 
-
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.cwen.notification_service.domain.NotificationService;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-
-
-import java.math.BigDecimal;
-import java.time.Instant;
 
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractIntegrationTest {
-
+    @SpyBean
+    protected NotificationService notificationService;
 
 }
