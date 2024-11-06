bcb993601a954f5771155b8ebb96baafdb64cf5b
Chu Hao Wen <chuhaow@gmail.com>
2024-11-06 16:19:14 -0600

    add nav layout and product page

--- bookstore-webapp/src/main/java/com/cwen/bookstore_webapp/controller/ProductController.java
+++ bookstore-webapp/src/main/java/com/cwen/bookstore_webapp/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.cwen.bookstore_webapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
@@ -11,7 +13,9 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    String productsPage(){
+    String productsPage(@RequestParam(name = "page", defaultValue = "1")int pageNo,
+                        Model model){
+        model.addAttribute("pageNo", pageNo);
         return "products";
     }
 }
---
+++ bookstore-webapp/src/main/resources/static/js/products.js
@@ -0,0 +1,21 @@
+document.addEventListener('alpine:init', ()=>{
+    Alpine.data('initData', (pageNo) => ({
+        pageNo: pageNo,
+        products:{
+            data:[]
+        },
+        init(){
+            this.loadProducts(pageNo)
+        },
+        loadProducts(pageNo){
+            console.log("loading products")
+            $.getJSON("http://localhost:8989/catalog/api/products?page="+pageNo, (response) =>{
+                console.log(response)
+                this.products=response;
+            })
+        },
+        addToCart(product){
+            console.log("Adding "+ product.name)
+        }
+    }))
+});
