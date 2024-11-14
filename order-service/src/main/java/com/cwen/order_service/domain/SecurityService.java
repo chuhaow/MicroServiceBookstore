21c140dab32e81ab8183176a302e6751dc0f4b75
Chu Hao Wen <chuhaow@gmail.com>
2024-11-14 15:48:56 -0600

    set up order service security

--- order-service/src/main/java/com/cwen/order_service/domain/SecurityService.java
+++ order-service/src/main/java/com/cwen/order_service/domain/SecurityService.java
@@ -1,11 +1,17 @@
 package com.cwen.order_service.domain;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityService {
 
     public String getLoginUserName(){
-        return "user";
+        JwtAuthenticationToken authenticationToken =
+                (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        Jwt jwt = (Jwt) authenticationToken.getPrincipal();
+        return jwt.getClaimAsString("preferred_username");
     }
 }
